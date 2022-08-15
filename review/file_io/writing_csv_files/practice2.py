24b3278cc1f7382e3c526ffea3168370f7ebe441
Roniel <ronielr192@gmail.com>
2022-08-15 09:26:29 -0300

    Create practice2.py

---
+++ review/file_io/writing_csv_files/practice2.py
@@ -0,0 +1,18 @@
+"""
+Review study2
+"""
+import csv
+
+header = ['name', 'area', 'country_code2', 'country_code3']
+data = [
+    ['Albania', 28748, 'AL', 'ALB'],
+    ['Algeria', 2381741, 'DZ', 'DZA'],
+    ['American Samoa', 199, 'AS', 'ASM'],
+    ['Andorra', 468, 'AD', 'AND'],
+    ['Angola', 1246700, 'AO', 'AGO']
+]
+
+with open('countries.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerows(data)
